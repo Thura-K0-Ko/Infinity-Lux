116af8a16fcd0516ceec6a37426840798b704e25
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-06 18:33:55 +0630

    fix: issue in public layout

--- src/features/Public/components/PublicLayout.jsx
+++ src/features/Public/components/PublicLayout.jsx
@@ -1,12 +1,11 @@
-import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-
 import Footer from "./Footer";
 import PageLoading from "../../../components/PageLoading";
+import { Suspense } from "react";
 
 const PublicLayout = () => {
   return (
-    <main className=" flex flex-col min-h-screen xl:m-w-[1366px] 2xl:max-w-[1512px] mx-auto overflow-hidden">
+    <main className=" flex flex-col min-h-screen xl:max-w-[1366px] 2xl:max-w-[1512px] mx-auto overflow-hidden">
       <Suspense fallback={<PageLoading />}>
         <Outlet />{" "}
       </Suspense>
