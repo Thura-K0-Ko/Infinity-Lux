1e88045d1fb2a2c0aeb5f1426330808dfc92afef
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-11 19:16:56 +0630

    feat: Update Blog Page

--- src/features/Public/components/BlogCard.jsx
+++ src/features/Public/components/BlogCard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import useBlogStore from "../../../stores/useBlogStore";
+import ArticleCard from "./ArticleCard";
+import Container from "../../../components/Container";
 
 const BlogCard = () => {
-  return <div>BlogCard</div>;
+  const { blogs } = useBlogStore();
+  return (
+    <div className="py-40">
+      <Container>
+        <div className=" grid grid-cols-1 md:grid-cols-3 gap-5">
+          {blogs.map((blog) => (
+            <ArticleCard key={blog.id} blog={blog} />
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default BlogCard;
