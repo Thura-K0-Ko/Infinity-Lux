555c16599e6e87162915dd96d08b0a229c23a251
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-22 19:39:17 +0630

    style: Update responsive tablet for hotel-article

--- src/features/Public/components/ArticleCard.jsx
+++ src/features/Public/components/ArticleCard.jsx
@@ -6,28 +6,30 @@ const ArticleCard = ({
   blog: { id, title, description, date, image, imgAlt },
 }) => {
   return (
-    <div className="">
-      <div className="  rounded-lg">
-        <img
-          src={image}
-          className=" h-[298px] w-full object-cover rounded-t-lg"
-          alt={imgAlt}
-        />
-        <div className="p-5 flex flex-col gap-5 bg-customStone-500 rounded-b-lg">
-          <h2 className=" font-heading text-[#141414] text-3xl font-normal">
-            {title}
-          </h2>
-          <p className="text-gray-500 text-xl font-medium">{description}</p>
-          <div className=" flex items-center justify-between">
-            <p className="text-[#141414] text-xl font-medium">{date}</p>
-            <Link
-              to={`/blog/blog-detail/${id}`}
-              className=" flex items-center gap-3 text-[#141414] text-xl font-medium p-2   transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 "
-            >
-              <span>Read More</span>
-              <HiArrowSmallRight className=" size-7" />
-            </Link>
-          </div>
+    <div className="  rounded-lg">
+      <img
+        src={image}
+        className=" h-[269.86px] xl:h-[298px] w-full object-cover rounded-t-lg"
+        alt={imgAlt}
+      />
+      <div className="p-5 flex flex-col gap-5 bg-customStone-500 rounded-b-lg">
+        <h2 className=" font-heading text-[#141414] text-2xl xl:text-3xl font-normal">
+          {title}
+        </h2>
+        <p className="text-gray-500 text-base xl:text-xl font-medium">
+          {description}
+        </p>
+        <div className=" flex items-center justify-between">
+          <p className="text-[#141414] text-lg xl:text-xl font-medium">
+            {date}
+          </p>
+          <Link
+            to={`/blog/blog-detail/${id}`}
+            className=" flex items-center gap-3 text-[#141414] text-lg xl:text-xl font-medium p-2   transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 "
+          >
+            <span>Read More</span>
+            <HiArrowSmallRight className=" w-[21.73px] xl:size-7" />
+          </Link>
         </div>
       </div>
     </div>
--- src/features/Public/components/HotelArticle.jsx
+++ src/features/Public/components/HotelArticle.jsx
@@ -4,22 +4,27 @@ import useBlogStore from "../../../stores/useBlogStore";
 
 const HotelArticle = () => {
   const { blogs } = useBlogStore();
-  console.log(blogs)
+  console.log(blogs);
   return (
     <section>
-      <div className=" py-40 bg-customGray-500 ">
+      <div className=" py-[90px] xl:py-40 bg-customGray-500 ">
         <Container>
           <div className="flex flex-col items-center gap-16">
             <div className=" flex flex-col items-center gap-6">
-              <p className=" text-customPurple-500 text-2xl font-medium">
+              <p className=" text-customPurple-500 text-base xl:text-2xl font-medium">
                 Latests News
               </p>
-              <h3 className="text-white text-5xl font-normal font-heading">
+              <h3 className="text-white text-[40px]  xl:text-5xl font-normal font-heading">
                 Hotel Articles
               </h3>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+            <div className="hidden xl:grid xl:grid-cols-3 gap-5">
               {blogs.slice(0, 3).map((blog) => (
+                <ArticleCard key={blog.id} blog={blog} />
+              ))}
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:hidden gap-5">
+              {blogs.slice(0, 2).map((blog) => (
                 <ArticleCard key={blog.id} blog={blog} />
               ))}
             </div>
