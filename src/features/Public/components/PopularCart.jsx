ca0718d76ad463728b23c2a42a87e65aa2a633fa
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-23 14:44:42 +0630

    style: Update tablet responsive for blog-detail Page

---
+++ src/features/Public/components/PopularCart.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PopularCart = ({ img, title, date }) => {
+  return (
+    <div className="  flex  items-center gap-5">
+      <img src={img} className=" h-[135px]" alt="" />
+      <div className="flex flex-col gap-5">
+        <h3 className="text-customGray-500 text-xl font-normal font-heading">
+          {title}
+        </h3>
+        <p className="text-gray-500 text-xl font-normal">{date}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PopularCart;
