b382f3696de3037bebce1a41f0882ff0cb460c35
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-23 13:30:04 +0630

    style: Update responsive for blog page

--- src/features/Public/components/BlogCard.jsx
+++ src/features/Public/components/BlogCard.jsx
@@ -6,9 +6,9 @@ import Container from "../../../components/Container";
 const BlogCard = () => {
   const { blogs } = useBlogStore();
   return (
-    <div className="py-40">
+    <div className=" py-[90px] xl:py-40">
       <Container>
-        <div className=" grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className=" grid grid-cols-2 xl:grid-cols-3 gap-5">
           {blogs.map((blog) => (
             <ArticleCard key={blog.id} blog={blog} />
           ))}
