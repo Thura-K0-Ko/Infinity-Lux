f7993ab7e602aa88ba8fb3237c9f06193fefff1d
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-23 10:27:41 +0630

    style: Update css responsive fro aboutus infinity

--- src/features/Public/components/AboutInfinityLux.jsx
+++ src/features/Public/components/AboutInfinityLux.jsx
@@ -15,68 +15,63 @@ const AboutInfinityLux = ({
   return (
     <section className="md:py-[90px] xl:py-40">
       <Container>
-        <div className="">
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 xl:gap-[108px]">
-            <div className={className}>
-              <div className=" flex flex-col gap-8">
-                <p className="text-customPurple-500 md:text-base xl:text-2xl font-medium ">
-                  About Infinity Lux
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 xl:gap-5">
+          <div className={className}>
+            <div className=" flex flex-col gap-8">
+              <p className="text-customPurple-500 md:text-base xl:text-2xl font-medium ">
+                About Infinity Lux
+              </p>
+              <div className=" flex flex-col gap-11 items-start">
+                <h1 className="font-heading text-customGray-500 md:text-[40px] xl:text-5xl font-normal dark:text-neutral-900 ">
+                  {title1}
+                  <br /> {title2}
+                </h1>
+                <p className=" md:text-base xl:text-xl text-gray-500 font-medium dark:text-gray-500 leading-relaxed xl:w-[542px]">
+                  {content}
                 </p>
-                <div className=" flex flex-col gap-11 items-start">
-                  <h1 className="font-heading text-customGray-500 md:text-[40px] xl:text-5xl font-normal dark:text-neutral-900 ">
-                    {title1}
-                    <br /> {title2}
-                  </h1>
-                  <p className=" md:text-base xl:text-xl text-gray-500 font-medium dark:text-gray-500 leading-relaxed xl:w-[542px]">
-                    {content}
-                  </p>
-                  {reservationText && (
-                    <Link
-                      to={"/login"}
-                      className="flex items-center gap-6"
-                    >
-                      <img
-                        src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/phone-call.svg"
-                        alt=""
-                        className="w-[56px] h-[56px]"
-                      />
-                      <p className="flex flex-col">
-                        <span className="font-heading text-2xl font-[400]">
-                          {reservationText}
-                        </span>
-                        <span className=" text-customPurple-500 text-2xl font-[400]">
-                          +959756895143{" "}
-                        </span>
-                      </p>
-                    </Link>
-                  )}
-                  {btnText && (
-                    <Link
-                      to={"about-us"}
-                      className="bg-customPurple-500 md:text-base xl:text-2xl font-semibold text-white md:px-5 md:py-2.5 xl:px-10 xl:py-5 transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 hover:bg-customPurple-800 focus:ring-4 focus:ring-customPurple-300 dark:bg-customPurple-600 dark:hover:bg-customPurple-700 focus:outline-none dark:focus:ring-customPurple-800"
-                    >
-                      {btnText}
-                    </Link>
-                  )}
-                </div>
+                {reservationText && (
+                  <Link to={"/login"} className="flex items-center gap-6">
+                    <img
+                      src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/phone-call.svg"
+                      alt=""
+                      className="w-[56px] h-[56px]"
+                    />
+                    <p className="flex flex-col">
+                      <span className="font-heading text-2xl font-[400]">
+                        {reservationText}
+                      </span>
+                      <span className=" text-customPurple-500 text-2xl font-[400]">
+                        +959756895143{" "}
+                      </span>
+                    </p>
+                  </Link>
+                )}
+                {btnText && (
+                  <Link
+                    to={"about-us"}
+                    className="bg-customPurple-500 md:text-base xl:text-2xl font-semibold text-white md:px-5 md:py-2.5 xl:px-10 xl:py-5 transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 hover:bg-customPurple-800 focus:ring-4 focus:ring-customPurple-300 dark:bg-customPurple-600 dark:hover:bg-customPurple-700 focus:outline-none dark:focus:ring-customPurple-800"
+                  >
+                    {btnText}
+                  </Link>
+                )}
               </div>
             </div>
+          </div>
 
-            <div className=" col-span-1 flex xl:justify-end gap-5 md:h-[580px] xl:h-auto">
-              <div className="  mt-auto">
-                <img
-                  src={firstImg}
-                  alt="Luxury hotel interior"
-                  className="md:h-[490.6px] xl:h-[402px]  object-cover shadow-lg"
-                />
-              </div>
-              <div className="  mb-auto">
-                <img
-                  src={secondImg}
-                  alt="Rooftop terrace view"
-                  className="md:h-[490.6px] xl:h-[402px]  object-cover shadow-lg"
-                />
-              </div>
+          <div className=" col-span-1 flex xl:justify-end gap-5 md:h-[580px] xl:h-auto">
+            <div className="  mt-auto">
+              <img
+                src={firstImg}
+                alt="Luxury hotel interior"
+                className="md:h-[490.6px] xl:h-[402px]  object-cover shadow-lg"
+              />
+            </div>
+            <div className="  mb-auto">
+              <img
+                src={secondImg}
+                alt="Rooftop terrace view"
+                className="md:h-[490.6px] xl:h-[402px]  object-cover shadow-lg"
+              />
             </div>
           </div>
         </div>
--- src/features/Public/components/AboutInfinityLuxAndReservation.jsx
+++ src/features/Public/components/AboutInfinityLuxAndReservation.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import AboutInfinityLux from "./AboutInfinityLux";
 import Container from "../../../components/Container";
 
@@ -19,47 +17,69 @@ const AboutInfinityLuxAndReservation = () => {
       />
 
       <Container>
-        <div className=" inline-flex  gap-[108px] py-[141px]">
-          <div className="w-[630px] py-7">
-            <div className=" flex gap-6 h-full">
-              <div className="mt-auto">
-                <img
-                  src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/About_Us_Story_image.png"
-                  alt=""
-                  className="w-[305px] items-end"
-                />
-              </div>
-              <div>
-                <img
-                  src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/About_Us_Story_image1.png"
-                  alt=""
-                  className="w-[305px]"
-                />
-              </div>
+        <div className=" pb-[90px] flex flex-col-reverse xl:grid xl:grid-cols-2 gap-10   xl:gap-5 xl:py-[141px]">
+          {/* <div className=" col-span-1 py-7 flex xl:justify-end gap-5 md:h-[580px] xl:h-auto">
+            <div className="mt-auto">
+              <img
+                src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/About_Us_Story_image.png"
+                alt=""
+                className="w-[305px] items-end"
+              />
+            </div>
+            <div>
+              <img
+                src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/About_Us_Story_image1.png"
+                alt=""
+                className="w-[305px] mb-auto"
+              />
+            </div>
+          </div> */}
+          <div className=" col-span-1 xl:py-7 flex xl:justify-end gap-5 md:h-[580px] xl:h-auto">
+            <div className="  mt-auto">
+              <img
+                // src={firstImg}
+                src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/About_Us_Story_image.png"
+                alt="Luxury hotel interior"
+                className="md:h-[490.6px] xl:h-[402px]  object-cover shadow-lg"
+              />
+            </div>
+            <div className="  mb-auto">
+              <img
+                // src={secondImg}
+                src="/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/About_Us_Story_image1.png"
+                alt="Rooftop terrace view"
+                className="md:h-[490.6px] xl:h-[402px]  object-cover shadow-lg"
+              />
             </div>
           </div>
-          <div className=" flex flex-col w-[542px] items-start gap-8">
-            <h1 className=" text-2xl text-customPurple-500">
-              About Infinity Lux
-            </h1>
-            <div className="flex flex-col gap-11">
-              <p className="text-[#141414] font-heading text-5xl"> Our Story</p>
-              <p className="text-[#6B7280] text-xl font-[300]">
-                The Infinity Lux, a beacon of luxury and refinement, stands as a
-                testament to timeless opulence and modern innovation.
-                Established in 2013, this landmark property was envisioned by
-                renowned architect Michael Hall, whose goal was to create a
-                sanctuary that combined classical beauty with contemporary
-                comfort.
+
+          <div className="col-span-1 xl:flex xl:justify-end">
+            <div className=" flex flex-col xl:w-[542px] gap-8 ">
+              <p className="text-customPurple-500 md:text-base xl:text-2xl font-medium ">
+                About Infinity Lux
               </p>
-              <p className="text-[#6B7280] text-xl font-[300]">
-                Over the decades, The Infinity Lux expanded its offerings,
-                incorporating state-of-the-art amenities while preserving its
-                heritage charm. Its signature grand ballroom became a favored
-                destination for elite events and grand celebrations. The hotel
-                has hosted dignitaries, celebrities, and world leaders, becoming
-                synonymous with excellence and exclusivity.
-              </p>
+              <div className="flex flex-col gap-11">
+                <p className="font-heading text-customGray-500 md:text-[40px] xl:text-5xl font-normal dark:text-neutral-900 ">
+                  {" "}
+                  Our Story
+                </p>
+                <p className=" md:text-base xl:text-xl text-gray-500 font-medium dark:text-gray-500 leading-relaxed xl:w-[542px]">
+                  The Infinity Lux, a beacon of luxury and refinement, stands as
+                  a testament to timeless opulence and modern innovation.
+                  Established in 2013, this landmark property was envisioned by
+                  renowned architect Michael Hall, whose goal was to create a
+                  sanctuary that combined classical beauty with contemporary
+                  comfort.
+                </p>
+                <p className=" md:text-base xl:text-xl text-gray-500 font-medium dark:text-gray-500 leading-relaxed xl:w-[542px]">
+                  Over the decades, The Infinity Lux expanded its offerings,
+                  incorporating state-of-the-art amenities while preserving its
+                  heritage charm. Its signature grand ballroom became a favored
+                  destination for elite events and grand celebrations. The hotel
+                  has hosted dignitaries, celebrities, and world leaders,
+                  becoming synonymous with excellence and exclusivity.
+                </p>
+              </div>
             </div>
           </div>
         </div>
