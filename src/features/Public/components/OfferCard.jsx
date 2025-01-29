3ab3e8f941602ca21ce9ebfe8bd6cbebec5dfb3a
Sai Khawn Kham <khawnkham222@gmail.com>
2025-01-29 23:38:28 +0630

    style: room page responsive for mobile

--- src/features/Public/components/OfferCard.jsx
+++ src/features/Public/components/OfferCard.jsx
@@ -7,13 +7,13 @@ const OfferCard = () => {
    return (
       <div className="bg-white py-[60px]">
          <Container>
-            <h5 className="text-center text-customPurple-500 text-2xl font-medium mb-6">Comfort & Relax</h5>
-            <h3 className="text-center text-customGray-500 font-heading text-5xl font-normal mb-[60px]">Exclusive Offers</h3>
+            <h5 className="text-center text-customPurple-500 text-base md:text-2xl font-medium mb-4 md:mb-6">Comfort & Relax</h5>
+            <h3 className="text-center text-customGray-500 font-heading text-[32px] md:text-5xl font-normal mb-[40px] md:mb-[60px]">Exclusive Offers</h3>
             <div className="w-full overflow-x-scroll hsb whitespace-nowrap">
                {offers.map((offer) => (
-                  <div key={offer.id} className="inline-block w-1/2 xl:w-1/3 h-[512px] p-8">
+                  <div key={offer.id} className="inline-block w-full md:w-1/2 xl:w-1/3 h-[512px] p-8">
                   <div style={offer.background} className="flex flex-col justify-between h-full bg-cover bg-no-repeat p-8">
-                     <div className={`${offer.discount && 'w-max bg-white text-customPurple-500 text-2xl font-semibold p-5'}`}>{offer.discount}</div>
+                     <div className={`${offer.discount && 'w-max bg-white text-customPurple-500 text-[14px] md:text-2xl font-semibold py-[10px] px-[18px] md:px-5 md:py-5'}`}>{offer.discount}</div>
                      <div className={`${offer.event && 'font-heading text-white text-4xl font-normal'}`}>{offer.event}</div>
                   </div>
                   </div>
--- src/features/Public/pages/RoomsPage.jsx
+++ src/features/Public/pages/RoomsPage.jsx
@@ -17,7 +17,7 @@ const RoomsPage = () => {
         // titles={["Rooms"]}
       />
       <Container>
-        <div className="py-40 flex flex-col gap-40">
+        <div className="py-10 md:py-[90px] xl:py-40 flex flex-col gap-10 md:gap-[90px] xl:gap-40">
           {rooms.map((room) => (
             <RoomCard key={room.id} room={room} />
           ))}
