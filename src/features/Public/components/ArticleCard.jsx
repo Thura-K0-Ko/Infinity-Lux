0c39c3debf3c54b51e6c5acf4944818dad221335
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-30 10:06:26 +0630

    fix: Add Pagination Ui in blog Page

--- src/features/Public/components/ArticleCard.jsx
+++ src/features/Public/components/ArticleCard.jsx
@@ -9,23 +9,23 @@ const ArticleCard = ({
     <div className="  rounded-lg min-w-[341px] md:min-w-[373px] xl:min-w-[412px]">
       <img
         src={image}
-        className=" h-[269.86px] xl:h-[298px] w-full object-cover rounded-t-lg"
+        className=" h-[246px] md:h-[269.86px] xl:h-[298px] w-full object-cover rounded-t-lg"
         alt={imgAlt}
       />
-      <div className="p-5 flex flex-col gap-4 md:gap-5 bg-customStone-500 rounded-b-lg">
-        <h2 className=" font-heading text-[#141414] text-[22px] md:text-2xl xl:text-3xl font-normal">
+      <div className=" p-4 md:p-5 flex flex-col gap-4 md:gap-[18px] xl:gap-5 bg-customStone-500 rounded-b-lg">
+        <h2 className=" font-heading text-customGray-500 text-2xl xl:text-3xl font-normal">
           {title}
         </h2>
         <p className="text-gray-500 text-base xl:text-xl font-medium">
           {description}
         </p>
         <div className=" flex items-center justify-between">
-          <p className="text-[#141414] text-base md:text-lg xl:text-xl font-medium">
+          <p className="text-customGray-500 text-base md:text-lg xl:text-xl font-medium">
             {date}
           </p>
           <Link
             to={`/blog/blog-detail/${id}`}
-            className=" flex items-center gap-3 text-[#141414] text-sm md:text-lg xl:text-xl font-medium p-2   transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 "
+            className=" flex items-center gap-3 text-customGray-500 text-sm md:text-lg xl:text-xl font-medium p-2   transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 "
           >
             <span>Read More</span>
             <HiArrowSmallRight className=" size-[19.87px] md:size-[21.73px] xl:size-7" />
--- src/features/Public/components/BlogCard.jsx
+++ src/features/Public/components/BlogCard.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import useBlogStore from "../../../stores/useBlogStore";
 import ArticleCard from "./ArticleCard";
 import Container from "../../../components/Container";
+import Pagination from "../../../components/Pagination";
 
 const BlogCard = () => {
   const { blogs } = useBlogStore();
   return (
     <div className=" py-[90px] xl:py-40">
       <Container>
-        <div className=" grid grid-cols-2 xl:grid-cols-3 gap-5">
+        <div className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
           {blogs.map((blog) => (
             <ArticleCard key={blog.id} blog={blog} />
           ))}
         </div>
+        <Pagination />
       </Container>
     </div>
   );
