b31871950f152943cde5bcd062521609aa3b6d91
Thura-K0-Ko <thurakoko09374@gmail.com>
2025-01-30 14:16:28 +0630

    style: Update mobile responsive for about-us

--- src/features/Public/components/LeadersOfHotel.jsx
+++ src/features/Public/components/LeadersOfHotel.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import Container from "../../../components/Container";
+import { Link } from "react-router-dom";
 
 const LeadersOfHotel = () => {
   const leaders = [
@@ -25,61 +26,88 @@ const LeadersOfHotel = () => {
         "/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/Leader_Image3.png",
       position: "Chief Operating Officer",
     },
+    {
+      id: 4,
+      name: "Milly Alcock",
+      image:
+        "/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/Leader_Image1.png",
+      position: "Chief Executive Officer",
+    },
+    {
+      id: 5,
+      name: "Frank Grillo",
+      image:
+        "/assets/Infinity Lux image Assets/8. About Us ( Infinity Lux )/Leader_Image2.png",
+      position: "CEO, Co Founder",
+    },
   ];
 
+  const carouselRef = useRef();
+  const nextSlide = () => {
+    if (carouselRef.current) {
+      const container = carouselRef.current;
+      const scrollAmount = container.clientWidth + 16; // Includes gap between slides
+      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+      console.log(carouselRef.current);
+    }
+  };
+
+  const prevSlide = () => {
+    if (carouselRef.current) {
+      const container = carouselRef.current;
+      const scrollAmount = container.clientWidth + 16; // Includes gap between slides
+      container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    }
+  };
   return (
     <section>
       <div className="bg-[#F2F0ED]">
         <Container>
-          <div className=" mx-auto flex justify-center items-center md:placeholder-cyan-900 py-[90px] xl:py-40">
-            <div className=" flex flex-col w-[1280px] gap-[40px] xl:gap-[60px]">
-              <div className="flex flex-col justify-center items-center gap-6">
+          <div className=" md:placeholder-cyan-900 py-[50px] md:py-[90px] xl:py-40 overflow-hidden">
+            <div className=" flex flex-col gap-[40px] xl:gap-[60px]">
+              <div className="flex flex-col justify-center items-center gap-4 md:gap-6">
                 <p className=" text-customPurple-500 text-base xl:text-2xl">
                   Meet The Team
                 </p>
-                <p className="font-heading text-customGray-500 text-[40px] xl:text-5xl">
+                <p className="font-heading text-customGray-500 text-[32px] md:text-[40px] xl:text-5xl">
                   Our Leaders
                 </p>
               </div>
-              <div className="grid grid-cols-2 xl:grid-cols-3 gap-5">
-                {leaders.slice(0, 3).map(({ id, name, image, position }) => (
-                  <>
+              <div
+                ref={carouselRef}
+                className="flex items-center gap-5 w-full hsb overflow-x-scroll"
+              >
+                {leaders.map(({ id, name, image, position }) => (
+                  <div key={id} className="flex flex-col bg-white">
                     {/* ----------for xl screen------------ */}
-                    <div
-                      key={id}
-                      className="hidden xl:flex xl:flex-col bg-white"
-                    >
-                      <img src={image} alt="" className="h-[467px]" />
-                      <div className="flex flex-col items-center gap-3 py-8">
-                        <p className="text-customGray-500 text-center font-heading text-[32px]">
-                          {name}
-                        </p>
-                        <p className="text-gray-500 text-2xl  text-center">
-                          {position}
-                        </p>
-                      </div>
+                    <img
+                      src={image}
+                      alt=""
+                      className="min-w-[342px] md:min-w-[374px] xl:min-w-[413px] "
+                    />
+                    <div className="flex flex-col items-center gap-2.5 md:gap-3 py-6 md:py-8">
+                      <h3 className="text-customGray-500 text-center font-heading text-[26px] md:text-[32px]">
+                        {name}
+                      </h3>
+                      <p className="text-gray-500 text-xl md:text-2xl  text-center">
+                        {position}
+                      </p>
                     </div>
-                  </>
-                ))}
-
-                {leaders.slice(0, 2).map(({ id, name, image, position }) => (
-                  <>
-                    {/* ----------for md screen------------ */}
-
-                    <div key={id} className=" flex flex-col xl:hidden bg-white">
-                      <img src={image} alt="" className="h-[422px]" />
-                      <div className="flex flex-col items-center gap-3 py-[30px]">
-                        <p className="text-customGray-500 text-center font-heading text-[29px]">
-                          {name}
-                        </p>
-                        <p className="text-gray-500 text-[22px]  text-center">
-                          {position}
-                        </p>
-                      </div>
-                    </div>
-                  </>
+                  </div>
                 ))}
               </div>
+            </div>
+            {/* -----carousel button--------- */}
+            <div className=" flex xl:hidden items-center justify-center gap-2 md:gap-3 mt-5 md:mt-12  ">
+              <button
+                onClick={prevSlide}
+                className=" cursor-pointer bg-gray-500 p-1 md:p-1.5 rounded-full transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 "
+              ></button>
+              <button className=" bg-gray-500   p-1 md:p-1.5 rounded-full transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 "></button>
+              <button
+                onClick={nextSlide}
+                className=" cursor-pointer bg-gray-500 p-1 md:p-1.5 rounded-full transition-all duration-300 hover:shadow-lg transform hover:-translate-y-0.5 "
+              ></button>
             </div>
           </div>
         </Container>
